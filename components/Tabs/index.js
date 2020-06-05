783f8052e809e2c7a74dffe5175f15312d7799db
Cameron Lares <cameronlares@gmail.com>
2020-06-05 16:26:51 -0400

    Completed Tabs

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,59 @@
 //
 //  Each tab should look like this:
 //    <div class="tab">topic here</div>
+
+
+const tabCreator = function(arr){
+    const tab = document.createElement("div") // New Element
+    tab.classList.add("tab") // New Class
+    tab.textContent = arr // Text Content
+
+    return tab
+}
+
+const newTab = document.querySelector("div.topics ")
+
+
+const tabs = () => {
+
+    
+    axios.get('https://lambda-times-backend.herokuapp.com/topics')
+  
+    .then(response =>{
+        const topic = response.data.topics
+
+        topic.forEach(arr =>{
+      newTab.appendChild(tabCreator(arr))
+        })
+       
+      
+       
+       
+        //console.log(topic.topics)
+         //Might need a function here
+
+    })
+    .catch(error => console.error(error));
+}
+
+
+tabs()
+
+
+
+
+// const fetchUsers = () => {
+
+//     followersArray.forEach(arr => {
+  
+//         axios.get(`https://api.github.com/users/${arr}`)
+//           .then(response => {
+//             const users = response.data;
+//             myCard.appendChild(cardCreator(users))
+//             //push axios. replace followersarray with arr in axios 
+//           })
+//       })
+//       .catch(error => console.error(error));
+//   }
+  
+//   fetchUsers()
